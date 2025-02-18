71f33667316d17d69b683c781a79f4c8c3255ef6
ANawle <149271449+ANawle@users.noreply.github.com>
2025-02-18 18:39:23 +0530

    Update spacelift_aws_integration.tf

--- spacelift_aws_integration.tf
+++ spacelift_aws_integration.tf
@@ -2,9 +2,23 @@ provider "spacelift" {
   api_token = var.spacelift_api_token
 }
 
-variable "account_id" {
-  default = lookup(var.env, "ACCOUNT_ID", "")
+data "spacelift_environment_variable" "account_id" {
+  stack_id = "your-spacelift-stack-id"
+  name     = "ACCOUNT_ID"
 }
+
+variable "account_id" {
+  description = "AWS Account ID"
+  type        = string
+  default     = ""
+}
+
+resource "spacelift_aws_integration" "aws_account" {
+  name        = "AWS Account ${data.spacelift_environment_variable.account_id.value}"
+  role_arn    = "arn:aws:iam::${data.spacelift_environment_variable.account_id.value}:role/Spacelift"
+  external_id = "spacelift-external-id"
+}
+
 
 resource "spacelift_aws_integration" "aws_account" {
   name        = "AWS Account ${var.account_id}"
